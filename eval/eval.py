9ebac455957d62865e7c6f86dfc142085b76fd8f
HaonanGuo <74052754+HaonanGuo@users.noreply.github.com>
2022-03-28 12:01:06 +0800

    Update eval.py

--- eval/eval.py
+++ eval/eval.py
@@ -13,15 +13,8 @@ def eval_net(net, loader, device,savename=''):
     net.eval()
     mask_type = torch.float32
     n_val = len(loader)  # the number of batch
-    tot = 0
     hist=0
     hist1=0
-    hist_lis=[0,0,0,0,0]
-    if savename!='':
-        savedir = '../predict/' + savename
-        if os.path.exists(savedir):
-            shutil.rmtree(savedir)
-        os.makedirs(savedir)
     for num,batch in enumerate(loader):
         if num%20==19:
             print('Validating ',num+1,'/',len(loader))
@@ -34,24 +27,6 @@ def eval_net(net, loader, device,savename=''):
         pred2 = (pred[0] > 0).float()
         hist+=fast_hist(pred1.flatten().cpu().detach().int().numpy(),true_masks.flatten().cpu().int().numpy(),2)
         hist1+=fast_hist(pred2.flatten().cpu().detach().int().numpy(),true_masks.flatten().cpu().int().numpy(),2)
-        for i in range(imgs.shape[0]):
-            if 'austin' in name[i]:
-                hist_lis[0]+=fast_hist(pred2[i].flatten().cpu().detach().int().numpy(),true_masks[i].flatten().cpu().int().numpy(),2)
-            elif 'chicago' in name[i]:
-                hist_lis[1] += fast_hist(pred2[i].flatten().cpu().detach().int().numpy(),true_masks[i].flatten().cpu().int().numpy(), 2)
-            elif 'kitsap' in name[i]:
-                hist_lis[2] += fast_hist(pred2[i].flatten().cpu().detach().int().numpy(),true_masks[i].flatten().cpu().int().numpy(), 2)
-            elif 'tyrol' in name[i]:
-                hist_lis[3] += fast_hist(pred2[i].flatten().cpu().detach().int().numpy(),true_masks[i].flatten().cpu().int().numpy(), 2)
-            elif 'vienna' in name[i]:
-                hist_lis[4] += fast_hist(pred2[i].flatten().cpu().detach().int().numpy(),true_masks[i].flatten().cpu().int().numpy(), 2)
-            else:
-                print('Error!')
-        if savename!='':
-            for i in range(len(name)):
-                Image.fromarray((pred2[i]*255).float().cpu().numpy().squeeze()).save(savedir+'/'+name[i].split('/')[-1][:-4]+'.tif')
-    for i in range(5):
-        print(hist_lis[i])
     IOU=(np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)))[-1]
     IOU2=(np.diag(hist1) / (hist1.sum(axis=1) + hist1.sum(axis=0) - np.diag(hist1)))[-1]
     print('IOU:', IOU)
